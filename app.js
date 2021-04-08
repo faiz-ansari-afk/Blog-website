00a54146a3d3c0cb959a7029e1e668bfdb2ff1cc
Mohd Faiz Ansari <79037672+faiz-ansari-afk@users.noreply.github.com>
2021-04-08 13:17:48 +0530

    added database to website....

--- app.js
+++ app.js
@@ -1,58 +1,96 @@
 const express = require("express");
-const ejs = require("ejs"); 
+const ejs = require("ejs");
 const app = express();
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://admin:admin@cluster0.vmdiy.mongodb.net/posts", {
+    // local database below
+// mongoose.connect("mongodb://localhost:27017/posts", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const _ = require("lodash");
 
-// showing dummy content on starting 
-const homeStartingContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, tenetur nisi cupiditate, eos saepe perferendis ex sequi, labore incidunt corporis non voluptatem excepturi veniam id quas debitis similique consequatur eligendi.";
-const aboutContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem asperiores ad consectetur quibusdam consequatur! A nihil expedita porro totam veniam odio quasi eum tenetur explicabo officia. Quis eaque facere aperiam?";
-const contactContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolore iure labore, veniam alias ipsum et ducimus, nesciunt deleniti amet neque porro delectus in sunt facilis praesentium nostrum obcaecati odit." ;
+// showing dummy content on starting
+const homeStartingContent =
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, tenetur nisi cupiditate, eos saepe perferendis ex sequi, labore incidunt corporis non voluptatem excepturi veniam id quas debitis similique consequatur eligendi.";
+// const aboutContent ="<h2>I'm Faiz</h2>";
+const contactContent =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolore iure labore, veniam alias ipsum et ducimus, nesciunt deleniti amet neque porro delectus in sunt facilis praesentium nostrum obcaecati odit.";
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+// creating schema of post db
+const post = {
+  title: String,
+  content: String,
+};
+// creating collections
+const Post = mongoose.model("Post", post);
 // global posts array
 let posts = [];
 
-app.get("/",(req,res)=>{
-    res.render("home",{homeStartContent:homeStartingContent,
-                        posts: posts
-    });
-});
-app.get("/about",(req,res)=>{
-    res.render("about",{aboutSampleText:aboutContent});
-});
-app.get("/contact-us",(req,res)=>{
-    res.render("contact",{contactText:contactContent});
-});
-app.get("/compose",(req,res)=>{
-    res.render("compose");
-});
-app.post("/compose",(req,res)=>{
-    let postTitle = req.body.postTitle;
-    let postContent = req.body.postBody;
-    let post ={
-        title: postTitle,
-        content: postContent
+app.get("/", (req, res) => {
+  Post.find({}, (err, foundItem) => {
+    if (!err) {
+      res.render("home", {
+        homeStartContent: homeStartingContent,
+        posts: foundItem,
+      });
     }
-    posts.push(post);
-    res.redirect("/");
-    // res.render("home",{postTitleUI:post.title,postContentUI:post.content});
-    
-})
-app.get("/post/:postName",(req,res)=>{
-    let requestedPostRoute = _.kebabCase(req.params.postName);
-    // str = str.trim().replace(" ","-").toLowerCase();
-    // requestedPostRoute = requestedPostRoute.trim().split(" ").join("-").toLowerCase();
-    posts.forEach(postContent =>{
-        let titleOfPostContent = _.kebabCase(postContent.title);
-        // console.log(titleOfPostContent);
-        if (titleOfPostContent === requestedPostRoute){
-        // console.log("match found");
-        res.render("post",{postTitle : postContent.title ,postContent: postContent.content });
-        }
-    });  
+  });
 });
-app.listen(process.env.PORT || 3000,()=>{
-    console.log("Server started at 3000");
-})
+app.get("/about", (req, res) => {
+  res.render("about"
+//   , { aboutSampleText: aboutContent }
+  );
+});
+app.get("/contact-us", (req, res) => {
+  res.render("contact");
+});
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+app.post("/compose", (req, res) => {
+  let postTitle = req.body.postTitle;
+  let postContent = req.body.postBody;
+  const post = new Post({
+    title: postTitle,
+    content: postContent,
+  });
+  // posts.push(post);
+  post.save();
+  res.redirect("/");
+  // res.render("home",{postTitleUI:post.title,postContentUI:post.content});
+});
+app.get("/post/:postName/:_id", (req, res) => {
+  let requestedPostRoute = _.kebabCase(req.params.postName);
+  const id = req.params._id;
+//   console.log(id);
+  // str = str.trim().replace(" ","-").toLowerCase();
+  // requestedPostRoute = requestedPostRoute.trim().split(" ").join("-").toLowerCase();
+  Post.findOne({_id: id},(err,foundPost)=>{
+      
+      if(!err){
+        res.render("post", {
+            postTitle: foundPost.title,
+            postContent: foundPost.content,
+          });
+      }else
+      console.log("Not found");
+  })
+//   posts.forEach((postContent) => {
+//     let titleOfPostContent = _.kebabCase(postContent.title);
+//     // console.log(titleOfPostContent);
+//     if (titleOfPostContent === requestedPostRoute) {
+//       // console.log("match found");
+//       res.render("post", {
+//         postTitle: postContent.title,
+//         postContent: postContent.content,
+//       });
+//     }
+//   });
+});
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started at 3000");
+});
